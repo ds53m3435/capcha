734c17ca99ed2a76faee37332f9f3c596d9acff8
Kantapon Budda <vongolareborn@live.com>
2016-10-10 03:54:22 +0700

    des st

--- spec/captcha.js
+++ spec/captcha.js
@@ -1,1 +1,9 @@
-captcha.js
+describe('Captcha App', () => {
+    describe('Pattern 1', () => {
+        ley pattern = 1;
+        it('should return "1+two" when input is 1,1,1,2', () => {
+            let app = new Captcha(pattern,1,1,2 )
+            expect(app.generate()).toEqual('1 + Two')
+        })
+    })
+})
