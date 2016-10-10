522be03f117e4fc6d50dd6b523e14111c4d73768
Kantapon Budda <vongolareborn@live.com>
2016-10-10 14:37:45 +0700

    edit 2 to 1

--- spec/captcha.spec.js
+++ spec/captcha.spec.js
@@ -1,7 +1,7 @@
 describe('Captcha App', () => {
     describe('Pattern 1', () => {
         ley pattern = 1;
-        it('should return "1+two" when input is 1,1,1,2', () => {
+        it('should return "1+two" when input is 1,1,1,1', () => {
             let app = new Captcha(pattern,1,1,1 )
             expect(app.generate()).toEqual('1 + One')
         })
