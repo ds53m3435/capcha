a6e8a2b219c499197c1afcf03d09fd38476815d1
Kantapon Budda <vongolareborn@live.com>
2016-10-10 14:37:02 +0700

    remove trash js

--- spec/captcha.js
+++
@@ -1,9 +0,0 @@
-describe('Captcha App', () => {
-    describe('Pattern 1', () => {
-        ley pattern = 1;
-        it('should return "1+two" when input is 1,1,1,2', () => {
-            let app = new Captcha(pattern,1,1,1 )
-            expect(app.generate()).toEqual('1 + Two')
-        })
-    })
-})
